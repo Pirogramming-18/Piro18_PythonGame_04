69dcf330596e5a25ab476486640eb68c4e976406
slsl2 <kimminji7153@gmail.com>
2023-01-09 00:40:19 +0900

    업다운 게임 수정

--- up_down.py
+++ up_down.py
@@ -9,63 +9,69 @@
 
 import random
 
+userList_Name = ['혜원', '성일', '민지', '창진'] #샘플. 나중에 지워야 함
 
-userList_Name = ['혜원', '성일', '민지', '창진'] #샘플
-
-def game02():
+def up_down():
     player_list = userList_Name
     loser_list = player_list
     
-    print("업앤다운 게임을 고르셨군요! 게임 시작~\n병뚜껑 안쪽에 적혀 있는 숫자를 맞혀 보세요! 1부터 50까지의 정수 중에 있습니다!")
+    print("Up&Down 게임을 고르셨군요! 병뚜껑 안쪽에 적혀 있는 숫자를 맞혀 보자!")
+    print("정답자 뺴고 전원 한잔 마시는 지옥의 게임... 1부터 50까지의 정수 중에 답이 있으니 맞혀 보세요~!")
     num_list = list(range(1, 51)) #편의를 위해 1~50 숫자 리스트 만들기
     answer = random.randint(1, 50) #정답 숫자 랜덤으로 선택
 
     while True:
         while True:
             try:
-                my_num = input("{}의 선택은: ".format(userList_Name[0]))
+                my_num = input("{}의 선택은?? ".format(player_list[0]))
                 if my_num.isdigit == False or int(my_num) not in num_list:
                     raise Exception
             except:
-                print("다시 입력해주세요!")
+                print("잘 생각해보고 다시 입력해주세요!")
             else:
                 break
         my_num = int(my_num)
 
         if my_num == answer:
-            print("˗ˋˏ 와 ˎˊ˗ 맞았습니다! {} 빼고 모두 한잔해~".format(userList_Name[0]))
-            loser_list.remove(userList_Name[0])
+            if len(player_list) == 2:
+                print("˗ˋˏ 와 ˎˊ˗ 맞았습니다!")
+                print("아 누가 술을 마셔~ {}(이)가 술을 마셔~ {}👏👏 👏👏 {}👏👏 👏👏 원~~샷!!!".format(player_list[1], player_list[1][0], player_list[1][1]))
+            else:
+                print("˗ˋˏ 와 ˎˊ˗ 맞았습니다!")
+                print("{} 빼고 모두 마~셔 마~셔 ٩( ᐛ )و 네발로 기~어~ (งᐖ)ว".format(player_list[0]))
+            loser_list.remove(player_list[0])
             break
         elif my_num < answer:
             del num_list[:num_list.index(my_num)+1]
             print("음... {}보다 Up!".format(str(my_num)))
-            #print(num_list)
         elif my_num > answer:
             del num_list[num_list.index(my_num):]
             print("음... {}보다 Down!".format(str(my_num)))
-            #print(num_list)
 
         n = 1
         for i in range(len(player_list)-1):
             num_choice = random.choice(num_list)
             num = num_choice
-            print("{}의 선택은: {}".format(player_list[n], num))
+            print("{}의 선택은?? {}".format(player_list[n], num))
             if num == answer:
-                print("˗ˋˏ 와 ˎˊ˗ 맞았습니다! {} 빼고 모두 한잔해~".format(player_list[n]))
+                if len(player_list) == 2:
+                    print("˗ˋˏ 와 ˎˊ˗ 맞았습니다!")
+                    print("아 누가 술을 마셔~ {}(이)가 술을 마셔~ {}👏👏 👏👏 {}👏👏 👏👏 원~~샷!!!".format(player_list[0], player_list[0][0], player_list[0][1]))
+                else:
+                    print("˗ˋˏ 와 ˎˊ˗ 맞았습니다!")
+                    print("{} 빼고 모두 마~셔 마~셔 ٩( ᐛ )و 네발로 기~어~ (งᐖ)ว".format(player_list[n]))
                 loser_list.remove(player_list[n])
                 break
             elif num < answer:
                 del num_list[:num_list.index(num)+1]
                 print("음... {}보다 Up!".format(str(num)))
-                #print(num_list)
             elif num > answer:
                 del num_list[num_list.index(num):]
                 print("음...{}보다 Down!".format(str(num)))
-                #print(num_list)
             n += 1
         if num == answer: break
         
     print("~~~~~게임 끝!!~~~~~")
-    print(loser_list) #확인용
+    print(loser_list) #확인용 #나중에 지워야 함
     
-game02()
+up_down()
